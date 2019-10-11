6bf687b77e999940f6cef2f78661bc19aeeefe72
kewal <kewal2123@gmail.com>
2019-10-11 14:39:45 -0400

    Basic recipes page struture working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is simple test','https://www.makingthymeforhealth.com/wp-content/uploads/2018/01/One-Pot-Spanish-Quinoa-_-700x1050.jpg')
   ]
@@ -15,4 +16,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe : Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
 }
